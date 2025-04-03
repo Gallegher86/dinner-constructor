4589e327d2f2e64cb9e10f9c0a615dbe103c5db9
Anton Riabushkin <riabushkinanton@yandex.ru>
2025-04-03 22:01:06 +0700

    Поправил замечания, оптимизировал код.

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -7,54 +7,36 @@ public class DinnerConstructor {
     HashMap<String, ArrayList<String>> dishTypes = new HashMap<>();
 
     void addNewDish (String dishType, String dishName) {
+        ArrayList<String> dishNames;
 
         if (dishTypes.containsKey(dishType)) {
-            ArrayList<String> dishNames = dishTypes.get(dishType);
-            dishNames.add(dishName);
-            dishTypes.put(dishType, dishNames);
+            dishNames = dishTypes.get(dishType);
         } else {
-            ArrayList<String> dishNames = new ArrayList<>();
-            dishNames.add(dishName);
-            dishTypes.put(dishType, dishNames);
+            dishNames = new ArrayList<>();
         }
-        System.out.println("Блюдо " + dishName + " добавлено в категорию " + dishType + ".");
+        dishNames.add(dishName);
+        dishTypes.put(dishType, dishNames);
     }
 
-    void makeCombo (int numberOfCombos, ArrayList<String> typesToRand) {
+    ArrayList<ArrayList<String>> makeCombo (int numberOfCombos, ArrayList<String> typesToRand) {
         Random rnd = new Random();
         ArrayList<ArrayList<String>> comboList = new ArrayList<>();
 
-        if (typesToRand.isEmpty()) {
-            System.out.println("В программу не введены типы блюд, для создания комбинаций, работа прекращена.");
-            return;
-        } else {
-            for (int i = 0; i < numberOfCombos; i++) {
-                ArrayList<String> comboDishes = new ArrayList<>();
+        for (int i = 0; i < numberOfCombos; i++) {
+            ArrayList<String> comboDishes = new ArrayList<>();
 
-                for (String type : typesToRand) {
-                    ArrayList<String> dishesToRand = dishTypes.get(type);
-                    String dish = dishesToRand.get(rnd.nextInt(dishesToRand.size()));
-                    comboDishes.add(dish);
-                }
-                comboList.add(comboDishes);
+            for (String type : typesToRand) {
+                ArrayList<String> dishesToRand = dishTypes.get(type);
+                String dish = dishesToRand.get(rnd.nextInt(dishesToRand.size()));
+                comboDishes.add(dish);
             }
+            comboList.add(comboDishes);
         }
-        System.out.println("Из типов блюд " + typesToRand + " сформированы следующие комбинации:");
-        for (int i = 0; i < comboList.size(); i++) {
-            ArrayList<String> dishes = comboList.get(i);
-            System.out.println("Комбо " + (i + 1));
-            System.out.println(dishes);
-        }
+        return comboList;
     }
 
     boolean checkType(String dishType) {
-        boolean isNoType = false;
 
-        if (!dishTypes.containsKey(dishType)) {
-            System.out.println("Категории блюд " + dishType + " нет в базе программы, пожалуйста введите " +
-                    "существующую категорию или пустую строку для завершения цикла");
-            isNoType = true;
-        }
-        return isNoType;
+        return !dishTypes.containsKey(dishType);
     }
 }
--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -17,14 +17,13 @@ public class Main {
             String command = scanner.nextLine();
 
             switch (command) {
-                case "1":
-                    addNewDish();
-                    break;
-                case "2":
-                    generateDishCombo();
-                    break;
-                case "3":
+                case "1" -> addNewDish();
+                case "2" -> generateDishCombo();
+                case "3" -> {
+                    System.out.println("Работа программы завершена.");
                     return;
+                }
+                default -> System.out.println("Введена неизвестная команда.");
             }
             System.out.println("-".repeat(20));
         }
@@ -39,34 +38,54 @@ public class Main {
     }
 
     private static void addNewDish() {
-        System.out.println("Введите тип блюда:");
-        String dishType = scanner.nextLine();
-        System.out.println("Введите название блюда:");
-        String dishName = scanner.nextLine();
+        System.out.print("Введите тип блюда: ");
+        String dishType = scanner.nextLine().trim();
+        System.out.print("Введите название блюда: ");
+        String dishName = scanner.nextLine().trim();
 
         dc.addNewDish(dishType, dishName);
+        System.out.println("Блюдо " + dishName + " добавлено в категорию " + dishType + ".");
     }
 
     private static void generateDishCombo() {
         System.out.println("Начинаем конструировать обед...");
 
-        System.out.println("Введите количество наборов, которые нужно сгенерировать:");
+        System.out.print("Введите количество наборов, которые нужно сгенерировать: ");
         int numberOfCombos = scanner.nextInt();
         scanner.nextLine();
 
+        while (numberOfCombos <= 0) {
+            System.out.print("Введен ноль, либо отрицательное число. Пожалуйста, повторите ввод количества наборов: ");
+            numberOfCombos = scanner.nextInt();
+            scanner.nextLine();
+        }
+
         System.out.println("В списке программы имеются следующие типы блюд: " + dc.dishTypes.keySet());
         System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). Для завершения ввода введите пустую строку");
-        String dishType = scanner.nextLine();
+        String dishType = scanner.nextLine().trim();
         ArrayList<String> typesToRand = new ArrayList<>();
 
         while (!dishType.isEmpty()) {
             if (dc.checkType(dishType)) {
-                dishType = scanner.nextLine();
+                System.out.println("Категории блюд " + dishType + " нет в базе программы.");
+                System.out.println("Пожалуйста, введите существующую категорию или пустую строку для завершения цикла");
             } else {
                 typesToRand.add(dishType);
-                dishType = scanner.nextLine();
+            }
+            dishType = scanner.nextLine().trim();
+        }
+
+        if (typesToRand.isEmpty()) {
+            System.out.println("В программу не введены типы блюд, для создания комбинаций, выполнение команды прекращено.");
+        } else {
+            ArrayList<ArrayList<String>> comboList = dc.makeCombo(numberOfCombos, typesToRand);
+
+            System.out.println("Из типов блюд " + typesToRand + " сформированы следующие комбинации:");
+            for (int i = 0; i < comboList.size(); i++) {
+                ArrayList<String> dishes = comboList.get(i);
+                System.out.println("Комбо " + (i + 1));
+                System.out.println(dishes);
             }
         }
-        dc.makeCombo(numberOfCombos, typesToRand);
     }
 }
